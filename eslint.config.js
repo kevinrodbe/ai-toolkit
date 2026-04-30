76404d8c473a9b6a469c920e441e19bf42fbafcc
kevinrodbe <kevinrodbe@users.noreply.github.com>
2026-04-29 23:47:44 -0500

    init

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+// @ts-check
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import eslint from '@eslint/js';
+import json from '@eslint/json';
+import tslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+import packageJson from 'eslint-plugin-package-json';
+import nxPlugin from '@nx/eslint-plugin';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default tslint.config(
+	{ ignores: ['**/dist/', '**/results/', '**/docs/', '**/coverage/', '**/*.d.ts'] },
+	prettier,
+	packageJson.configs.recommended,
+	{
+		files: ['**/*.json'],
+		ignores: ['**/package-lock.json', '**/package.json'],
+		language: 'json/json',
+		...json.configs.recommended,
+		rules: {
+			...json.configs.recommended.rules,
+			'json/sort-keys': 'error',
+		},
+	},
+	{
+		extends: [eslint.configs.recommended, tslint.configs.recommended, tslint.configs.stylistic],
+		files: ['**/*.js', '**/*.ts'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			parserOptions: {
+				project: ['./tsconfig.json', './packages/skills/*/tsconfig.json'],
+				tsconfigRootDir: __dirname,
+			},
+			sourceType: 'module',
+		},
+		rules: {
+			'@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true }],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+				},
+			],
+			'sort-imports': [
+				'error',
+				{
+					allowSeparatedGroups: true,
+					ignoreCase: false,
+					ignoreDeclarationSort: true,
+					ignoreMemberSort: false,
+					memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple'],
+				},
+			],
+			'sort-keys': [
+				'error',
+				'asc',
+				{
+					caseSensitive: false,
+					minKeys: 2,
+					natural: false,
+				},
+			],
+		},
+	},
+	{
+		plugins: {
+			'@nx': nxPlugin,
+		},
+	}
+);
---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+	$schema: 'https://json.schemastore.org/prettierrc',
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	endOfLine: 'lf',
+};
+
+export default config;
